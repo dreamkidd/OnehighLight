e0a062252ba2e7f214e8bf18b8cba8e742499709
zhangyang <zhangyang04@duxiaoman.com>
2020-07-21 19:57:00 +0800

    fix: 🐛 config code sinnp

--- src/taskpane/codebuilder/codeBuilder.ts
+++ src/taskpane/codebuilder/codeBuilder.ts
@@ -1,6 +1,6 @@
 import hljs from "highlight.js";
-import DefaultColorTheme from "../theme/DefaultColorTheme"
-import ColorTheme from "../theme/IColorTheme"
+import DefaultColorTheme from "../theme/DefaultColorTheme";
+import ColorTheme from "../theme/IColorTheme";
 
 export default class CodeBuilder {
   codeSnippet: string;
@@ -9,7 +9,9 @@ export default class CodeBuilder {
 
   language: string;
 
-  _sourceCode: string;
+  private _sourceCode: string;
+
+  private _content: string;
 
   constructor(sourceCode: string, language = "auto", theme = new DefaultColorTheme()) {
     this._sourceCode = sourceCode;
@@ -17,8 +19,40 @@ export default class CodeBuilder {
     this.theme = theme;
   }
 
-   highLightCode(){
-
+  highLight() {
+    if (this.language === "auto") {
+      this._content = hljs.highlightAuto(this._sourceCode).value;
+      this.genBr();
+      this.replaceWriteSpace();
+      console.log(this._content);
+      this.codeSnippet = this._content;
+    }
   }
 
+  private genBr() {
+    this._content = this._content
+              .replace(/\r\n/g, "<br/>")
+              .replace(/\n\r/g, "<br/>")
+              .replace(/\n/g, "<br/>")
+              .replace(/\r/g, "<br/>")
+  }
+
+  private replaceWriteSpace() {
+    let content = this._content;
+    let idx = content.search(/>\s+/);
+    while (idx != -1) {
+      var count = 1;
+      while (/\s/.test(content.charAt(++idx))) {
+        count++;
+      }
+      let tokenized = ">";
+      while (count != 0) {
+        tokenized += "&nbsp;";
+        count--;
+      }
+      content = content.replace(/>\s+/, tokenized);
+      idx = content.search(/>\s+/);
+    }
+    this._content = content;
+  }
 }
--- test/codeBuilderTest.ts
+++ test/codeBuilderTest.ts
@@ -4,14 +4,18 @@ import CodeBuilder from "../src/taskpane/codebuilder/codeBuilder";
 describe("CodeBuilder", () => {
   it("can be initialized with only sourceCode parameter", () => {
     let code = `
-        @Override
-        public Boolean batchDeleteInvoice(List<InvoiceNoInfo> infos) {
-            invoiceInfoService.batchDelete(infos);
-            return true;
-        }
+    private genBr() {
+        this._content = this._content
+                  .replace(/\r\n/g, "<br/>")
+                  .replace(/\n\r/g, "<br/>")
+                  .replace(/\n/g, "<br/>")
+                  .replace(/\r/g, "<br/>")
+          // .trim();
+    }
         `;
     const builder = new CodeBuilder(code);
     expect(builder.language).to.eq("auto");
     expect(builder.theme.name).to.eq("Default");
+    builder.highLight()
   });
 });
